288a4e38b8e0d171000b3886c6df71483a75268e
gavalishubham94 <gavalishubham94@gmail.com>
2022-08-07 11:32:43 +0530

    employee controlleradded

---
+++ src/main/java/com/shubham/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.shubham.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	public void addEmployee() {
+		System.out.println("employee added");
+	}
+	
+}
